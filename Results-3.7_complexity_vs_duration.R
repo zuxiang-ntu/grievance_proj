31297c1260f35c87345a1532297b44ca7776333c
Zu Xiang <zuxiang.ng@ntu.edu.sg>
2022-11-17 15:46:49 +0800

    reorganise code so that figures and tables for manuscript are first. removed code for old figures

--- Results-3.7_complexity_vs_duration.R
+++ Results-3.7_complexity_vs_duration.R
@@ -1,7 +1,7 @@
 ## 3.7 Complexity and characteristics vs. Duration
 
-# Figure 10. Grievance complexity vs Duration
-# Stats table object
+# Figure 9a. Complexity vs duration
+# Stats table object to plot median and mean
 stats_complexity_dur <- table_analysis %>% 
   filter(Status=="Closed", Complexity != "NA") %>%
   group_by(Complexity) %>% 
@@ -9,18 +9,29 @@ stats_complexity_dur <- table_analysis %>%
             Mean = round((mean(Duration_lodged_closed, na.rm = T)), digits=1)) %>%
   melt(id.vars="Complexity")
 # Figure
-table_analysis %>% 
+plot_complexity_duration <- 
+  table_analysis %>% 
   filter(Status=="Closed", Complexity != "NA") %>%
   ggplot(aes(x = Complexity, y = Duration_lodged_closed)) + 
   geom_jitter(position = position_jitter(0.2, 0.2), alpha=0.2) +
   geom_line(data= stats_complexity_dur,
             aes(x=Complexity, y=value, linetype=variable), size=1) + 
-  scale_x_continuous(n.breaks= 6) +
-  labs(x = "Complexity score", y = "Duration (months)", linetype="") +
+  scale_x_continuous(n.breaks= 8) +
+  labs(x = "", y = "Duration (months)", linetype="", title= "(a)") +
   theme(axis.title = element_text(size = 12), axis.text = element_text(size=11.5))
+# sample size
+table_analysis %>% 
+  filter(Status=="Closed", Complexity != "NA") %>%
+  select(Complexity, Duration_lodged_closed) %>% 
+  na.omit() %>% nrow()
+# Regression stats
+lm(Duration_lodged_closed ~ Complexity, 
+   table_analysis %>% 
+     filter(Status=="Closed")) %>% 
+  summary()
 
-# Figure 11. Grievance characteristics vs Duration
-# Stats table for clarity vs Duration
+# Figure 9b. Allegation clarity vs duration
+# Stats table to plot median and mean
 stats_clarity_dur <-
   table_analysis %>% 
   filter(Status=="Closed", GC_clarity != "NA") %>%
@@ -28,34 +39,7 @@ stats_clarity_dur <-
   summarise(Median = round((median(Duration_lodged_closed, na.rm = T)), digits=1),
             Mean = round((mean(Duration_lodged_closed, na.rm = T)), digits=1)) %>%
   melt(id.vars="GC_clarity")
-# Stats table for Grievance raiser status vs Duration
-stats_status_dur <- 
-  table_analysis %>% 
-  filter(Status=="Closed", GC_status != "NA") %>% 
-  mutate(GC_status = factor(GC_status, levels=c("Internal reporting", "High profile", "Low profile"))) %>%
-  group_by(GC_status) %>% 
-  summarise(Median = round((median(Duration_lodged_closed, na.rm = T)), digits=1),
-            Mean = round((mean(Duration_lodged_closed, na.rm = T)), digits=1)) %>% 
-  melt(id.vars="GC_status")
-# Stats table for number of aggrieved
-stats_number_dur <-
-  table_analysis %>% 
-  filter(Status=="Closed", !GC_number %in% c("4", "NA")) %>%
-  group_by(GC_number) %>%  
-  summarise(Median = round((median(Duration_lodged_closed, na.rm = T)), digits=1),
-            Mean = round((mean(Duration_lodged_closed, na.rm = T)), digits=1)) %>% 
-  melt(id.vars="GC_number")
-# Stats table for sourcing
-stats_sourcing_dur <- 
-  table_analysis %>% 
-  filter(Status=="Closed", !GC_sourcing %in% c("NA", "Unable to determine")) %>%
-  mutate(GC_sourcing = factor(GC_sourcing, levels = c("Not in supply chain", "Vertical", "Direct", "Indirect"))) %>%
-  group_by(GC_sourcing) %>%
-  summarise(Median = round((median(Duration_lodged_closed, na.rm = T)), digits=1),
-            Mean = round((mean(Duration_lodged_closed, na.rm = T)), digits=1)) %>%
-  melt(id.vars="GC_sourcing")
-
-# Allegation clarity vs Duration figure 
+# Figure
 plot_clarity_duration <-
   table_analysis %>% 
   filter(Status=="Closed", GC_clarity != "NA") %>%
@@ -64,10 +48,29 @@ plot_clarity_duration <-
   geom_line(data= stats_clarity_dur, 
             aes(x= GC_clarity, y= value, group= variable, linetype= variable),
             size= 1) +
-  labs(x= "", y= "Duratiion (months)", linetype= "", title= "(a)") +
+  labs(x= "", y= "Duratiion (months)", linetype= "", title= "(b)") +
   theme(axis.title = element_text(size = 12), axis.text = element_text(size=11.5))
+# sample size
+table_analysis %>% 
+  filter(Status=="Closed", GC_clarity != "NA") %>% 
+  select(GC_clarity, Duration_lodged_closed) %>%
+  na.omit() %>% nrow()
+# Regression stats
+lm(Duration_lodged_closed ~ GC_clarity, 
+   table_analysis %>% filter(Status=="Closed", GC_clarity != "NA")) %>% 
+  summary() 
 
-# Status of grievance raiser vs Duration figure
+# Figure 9c. Status of grievance raiser vs Duration
+# Stats table to plot median and mean
+stats_status_dur <- 
+  table_analysis %>% 
+  filter(Status=="Closed", GC_status != "NA") %>% 
+  mutate(GC_status = factor(GC_status, levels=c("Internal reporting", "High profile", "Low profile"))) %>%
+  group_by(GC_status) %>% 
+  summarise(Median = round((median(Duration_lodged_closed, na.rm = T)), digits=1),
+            Mean = round((mean(Duration_lodged_closed, na.rm = T)), digits=1)) %>% 
+  melt(id.vars="GC_status")
+# Figure
 plot_status_duration <-
   table_analysis %>% 
   mutate(GC_status = factor(GC_status, levels=c("Internal reporting", "High profile", "Low profile"))) %>%
@@ -78,10 +81,29 @@ plot_status_duration <-
             aes(x= GC_status, y= value, group= variable, linetype= variable),
             size= 1) +
   scale_x_discrete(labels= scales::label_wrap(10)) +
-  labs(x= "", y= "Duratiion (months)", linetype= "", title= "(b)") +
+  labs(x= "", y= "Duratiion (months)", linetype= "", title= "(c)") +
   theme(axis.title = element_text(size = 12), axis.text = element_text(size=11.5))
+# sample size
+table_analysis %>% 
+  filter(Status=="Closed", GC_status != "NA") %>% 
+  select(GC_status, Duration_lodged_closed) %>% 
+  na.omit() %>% nrow()
+# Regression stats
+lm(Duration_lodged_closed ~ GC_status, 
+   table_analysis %>% 
+     mutate(GC_status = factor(GC_status, levels=c("Internal reporting", "High profile", "Low profile")))) %>% 
+  summary()
 
-# Number of aggrieved parties vs Duration
+# Figure 9d. Number of aggrieved parties vs Duration
+# Stats table to plot median and mean
+stats_number_dur <-
+  table_analysis %>% 
+  filter(Status=="Closed", !GC_number %in% c("4", "NA")) %>%
+  group_by(GC_number) %>%  
+  summarise(Median = round((median(Duration_lodged_closed, na.rm = T)), digits=1),
+            Mean = round((mean(Duration_lodged_closed, na.rm = T)), digits=1)) %>% 
+  melt(id.vars="GC_number")
+# Figure 9d
 plot_aggrieved_duration <-
   table_analysis %>% 
   filter(Status=="Closed", !GC_number %in% c("4", "NA")) %>%
@@ -90,10 +112,30 @@ plot_aggrieved_duration <-
   geom_line(data= stats_number_dur, 
             aes(x= GC_number, y= value, group= variable, linetype= variable),
             size= 1) +
-  labs(x= "", y= "Duratiion (months)", linetype= "", title= "(c)") +
+  labs(x= "", y= "Duratiion (months)", linetype= "", title= "(d)") +
   theme(axis.title = element_text(size = 12), axis.text = element_text(size=11.5))
+# sample size
+table_analysis %>% 
+  filter(Status=="Closed") %>%
+  select(GC_number, Duration_lodged_closed) %>%
+  na.omit() %>% nrow()
+# Regression stats
+lm(Duration_lodged_closed ~ GC_number,
+   table_analysis %>% 
+     filter(Status=="Closed", !GC_number %in% c("4", "NA"))) %>% 
+  summary()
 
-# Sourcing relationship vs Duration
+# Figure 9e. Sourcing relationship vs Duration
+# Stats table to plot median and mean
+stats_sourcing_dur <- 
+  table_analysis %>% 
+  filter(Status=="Closed", !GC_sourcing %in% c("NA", "Unable to determine")) %>%
+  mutate(GC_sourcing = factor(GC_sourcing, levels = c("Not in supply chain", "Vertical", "Direct", "Indirect"))) %>%
+  group_by(GC_sourcing) %>%
+  summarise(Median = round((median(Duration_lodged_closed, na.rm = T)), digits=1),
+            Mean = round((mean(Duration_lodged_closed, na.rm = T)), digits=1)) %>%
+  melt(id.vars="GC_sourcing")
+# Figure
 plot_sourcing_duration <-
   table_analysis %>% 
   filter(Status=="Closed", !GC_sourcing %in% c("NA", "Unable to determine")) %>%
@@ -104,127 +146,77 @@ plot_sourcing_duration <-
             aes(x= GC_sourcing, y= value, group= variable, linetype= variable),
             size= 1) +
   scale_x_discrete(labels= scales::label_wrap(10)) +
-  labs(x= "", y= "Duratiion (months)", linetype= "", title= "(d)") +
+  scale_x_discrete(labels=c("Non-supplier", "Vertical", "Direct", "Indirect")) +
+  labs(x= "", y= "Duratiion (months)", linetype= "", title= "(e)") +
   theme(axis.title = element_text(size = 12), axis.text = element_text(size=11.5))
+# sample size
+table_analysis %>% 
+  filter(Status=="Closed", !GC_sourcing %in% c("NA", "Unable to determine")) %>%
+  select(GC_sourcing, Duration_lodged_closed) %>% 
+  na.omit() %>% nrow()
+# Regression stats
+lm(Duration_lodged_closed ~ GC_sourcing, 
+   table_analysis %>% 
+     filter(Status=="Closed", !GC_sourcing %in% c("NA", "Unable to determine")) %>%
+     mutate(GC_sourcing = factor(GC_sourcing, levels = c("Not in supply chain", "Vertical", "Direct", "Indirect")))) %>% 
+  summary()
 
-# Combined Figure 11 plot
-ggarrange(plot_clarity_duration, plot_status_duration, plot_aggrieved_duration, plot_sourcing_duration, 
-          ncol=2, nrow=2, common.legend = T, align='hv')
+# Figure 9f. Grievance theme vs Duration
+# Stats table to plot median and mean
+stats_theme_dur <- 
+  table_analysis %>% 
+  filter(Status=="Closed") %>%
+  mutate(Grievance_theme = factor(Grievance_theme, levels = c("Environmental", "Social", "Both"))) %>%
+  group_by(Grievance_theme) %>%
+  summarise(Median = round((median(Duration_lodged_closed, na.rm = T)), digits=1),
+            Mean = round((mean(Duration_lodged_closed, na.rm = T)), digits=1)) %>%
+  melt(id.vars="Grievance_theme")
+# Figure
+plot_theme_duration <-
+  table_analysis %>% 
+  filter(Status=="Closed") %>%
+  mutate(Grievance_theme = factor(Grievance_theme, levels = c("Environmental", "Social", "Both"))) %>%
+  ggplot(aes(x = Grievance_theme, y = Duration_lodged_closed)) + 
+  geom_jitter(position = position_jitter(0.2, 0.2), alpha=0.2) +
+  geom_line(data= stats_theme_dur, 
+            aes(x= Grievance_theme, y= value, group= variable, linetype= variable),
+            size= 1) +
+  labs(x= "", y= "Duratiion (months)", linetype= "", title= "(f)") +
+  theme(axis.title = element_text(size = 12), axis.text = element_text(size=11.5))
+# sample size
+table_analysis %>% 
+  filter(Status=="Closed") %>%
+  select(Grievance_theme, Duration_lodged_closed) %>% 
+  na.omit() %>% nrow()
+# Regression stats
+lm(Duration_lodged_closed ~ Grievance_theme, 
+   table_analysis %>% 
+     filter(Status=="Closed") %>%
+     mutate(Grievance_theme = factor(Grievance_theme, levels = c("Environmental", "Social", "Both")))) %>% 
+  summary()
+
+# Combined Figure 9 plot
+ggarrange(plot_complexity_duration, plot_clarity_duration, plot_status_duration, 
+          plot_aggrieved_duration, plot_sourcing_duration, plot_theme_duration,
+          ncol=2, nrow=3, common.legend = T)
 #####
-# Alt plot, complexity vs duration
+# Code for additional info
+# Alt plot complexity v. duration, fitted linear regression line
 table_analysis %>% 
   filter(Status=="Closed", Complexity != "NA") %>%
   ggplot(aes(x = Complexity, y = Duration_lodged_closed)) + 
   geom_jitter(position = position_jitter(0.2, 0.2), alpha=0.2) +
-  geom_point(data= stats_complexity_dur,
-             aes(x=Complexity, y=value, color=variable), 
-             size=2, show.legend=F) +
-  geom_line(data= stats_complexity_dur,
-            aes(x=Complexity, y=value, color=variable), size=1) + 
-  annotate("text", 
-           x= c(3:8)+0.2, y= c(4.8, 5.2, 13.2, 13, 9.9, 38.8)-2, 
-           label= c('4.8', '5.2', '13.2', '13', '9.9', '38.8'), color="blue", fontface= "bold", size= 4.5) +
-  annotate("text", 
-           x=c(2.8, 3.8, 4.8, 5.8, 6.8, 8.2), y=c(8.9, 9.8, 20.5, 17.3, 25.3, 30.3), 
-           label= c('6.9', '7.8', '18.5', '15.3', '23.3', '32.3'), color= "orange", fontface= "bold", size= 4.5) +
-  scale_color_manual(values=c("blue", "orange")) + 
+  geom_smooth(method= "lm", se= F, color="red", size= 1) +
   scale_x_continuous(n.breaks= 6) +
-  labs(x = "Complexity score", y = "Duration (months)", color="") +
+  scale_color_discrete(labels= "Regression line") +
+  labs(x= "Complexity score", y="Duration (months)", title="(b)") +
   theme(axis.title = element_text(size = 12), axis.text = element_text(size=11.5))
-# Alt plot, clarity vs duration
-table_analysis %>% 
-  filter(Status=="Closed", GC_clarity != "NA") %>%
-  ggplot(aes(x= GC_clarity, y = Duration_lodged_closed)) + 
-  geom_jitter(position = position_jitter(0.2, 0.2), alpha=0.2) +
-  geom_line(data= stats_clarity_dur, 
-            aes(x= GC_clarity, y= value, group= variable, linetype= variable),
-            size= 1) +
-  geom_point(data= stats_clarity_dur %>% filter(variable=="Median"), 
-             aes(x= GC_clarity, y= value, color= as.factor(value)),
-             size= 2) +
-  geom_point(data= stats_clarity_dur %>% filter(variable=="Mean"), 
-             aes(x= GC_clarity, y= value, fill=as.factor(value)),
-             size= 2, shape= 21) +
-  scale_color_manual(values = c("#92C5DE", "#2166AC"), 
-                     label= c("Clear: 9.4", "Unclear: 12.5")) +
-  scale_fill_manual(values= c("#F4A582", "#B2182B"), 
-                    label= c("Clear: 16.1", "Unclear: 21.5")) +
-  labs(x = "", y = "Duration (months)", title="(a) Allegation clarity", color="Median", fill="Mean", linetype="") +
-  guides(linetype= guide_legend(order=1), color= guide_legend(order=2), fill= guide_legend(order=3)) +
-  coord_cartesian(clip = 'off') +
-  theme(axis.title = element_text(size = 12), axis.text = element_text(size=11.5))
-# Alt plot, status vs duration
-table_analysis %>% 
-  mutate(GC_status = factor(GC_status, levels=c("Internal reporting", "High profile", "Low profile"))) %>%
-  filter(Status=="Closed", GC_status != "NA") %>%
-  ggplot(aes(x = GC_status, y = Duration_lodged_closed)) + 
-  geom_jitter(position = position_jitter(0.2, 0.2), alpha=0.2) +
-  geom_point(data= stats_status_dur %>% filter(variable=="Median"), 
-             aes(x= GC_status, y= value, color= as.character(value)),
-             size= 2) +
-  geom_point(data= stats_status_dur %>% filter(variable=="Mean"), 
-             aes(x= GC_status, y= value, fill= as.character(value)),
-             size= 2, shape= 21) +
-  geom_line(data= stats_status_dur, 
-            aes(x= GC_status, y= value, group= variable, linetype= variable),
-            size= 1) +
-  scale_color_manual(values = c("#2166AC", "#4393C3", "#92C5DE"),
-                     breaks = c(29, 11.2, 8.3), 
-                     label= c("Internal reporting: 29", "High profile: 11.2", "Low profile: 8.3")) +
-  scale_fill_manual(values= c("#F4A582", "#D6604D", "#B2182B"), 
-                    breaks= c(23.5, 16.5, 16.5), 
-                    label= c("Internal reporting: 23.5", "High profile: 16.5", "Low profile: 16.5")) +
-  labs(x="", y="Duration (months)", title="(b) Status of grievance raiser", col="Median", fill= "Mean", linetype="") +
-  guides(linetype= guide_legend(order=1), color= guide_legend(order=2), fill= guide_legend(order=3)) +
-  coord_cartesian(clip = 'off') +
-  theme(axis.title = element_text(size = 12), axis.text = element_text(size=11.5))
-# Alt plot, number of aggrieved vs duration
-table_analysis %>% 
-  filter(Status=="Closed", !GC_number %in% c("4", "NA")) %>%
-  ggplot(aes(x = as.factor(GC_number), y = Duration_lodged_closed)) + 
-  geom_jitter(position = position_jitter(0.2, 0.2), alpha=0.1) +
-  geom_point(data= stats_number_dur %>% filter(variable=="Median"), 
-             aes(x= GC_number, y= value, color= as.factor(value)),
-             size= 2.5) +
-  geom_point(data= stats_number_dur %>% filter(variable=="Mean"),
-             aes(x= GC_number, y= value, fill= as.factor(value)),
-             size= 2.5, shape= 21) +  
-  geom_line(data= stats_number_dur, 
-            aes(x= GC_number, y= value, group= variable, linetype= variable),
-            size= 1) +
-  scale_color_manual(breaks = c(19.8, 10.2, 13.2, 11.2),
-                     label= c("0 party: 19.8", "1 party: 10.2", "2 parties: 13.2", "3 parties: 11.2"),
-                     values= c("#D1E5F0", "#92C5DE", "#4393C3", "#2166AC")) +
-  scale_fill_manual(breaks= c(19.8, 17.3, 13.3, 21.3),
-                    labels= c("0 party: 19.8", "1 party: 17.3", "2 parties: 13.3", "3 parties: 21.3"),
-                    values= c("#B2182B", "#D6604D", "#F4A582", "#FDDBC7")) +
-  guides(linetype= guide_legend(order=1), color= guide_legend(order=2), fill= guide_legend(order=3)) +
-  labs(x="", y="Duration (months)", title="(c) Number of aggrieved parties",
-       color="Median", fill= "Mean", linetype="") +
-  coord_cartesian(clip = 'off') +
-  theme(axis.title = element_text(size = 12), axis.text = element_text(size=11.5))
-# Alt plot, sourcing vs duration
-table_analysis %>% 
-  filter(Status=="Closed", !GC_sourcing %in% c("NA", "Unable to determine")) %>%
-  mutate(GC_sourcing = factor(GC_sourcing, levels = c("Not in supply chain", "Vertical", "Direct", "Indirect"))) %>%
-  ggplot(aes(x = GC_sourcing, y = Duration_lodged_closed)) + 
-  geom_jitter(position = position_jitter(0.2, 0.2), alpha=0.1) +
-  geom_point(data= stats_sourcing_dur %>% filter(variable=="Median"), 
-             aes(x= GC_sourcing, y= value, color= as.factor(value)),
-             size= 2) +
-  geom_point(data= stats_sourcing_dur %>% filter(variable=="Mean"), 
-             aes(x= GC_sourcing, y= value, fill= as.factor(value)),
-             size= 2, shape= 21) +
-  geom_line(data= stats_sourcing_dur, 
-            aes(x= GC_sourcing, y= value, group= variable, linetype= variable),
-            size= 1) +
-  scale_color_manual(values= c("#D1E5F0", "#92C5DE", "#4393C3", "#2166AC"), 
-                     breaks= c(5.1, 2.7, 13.2, 11.4),
-                     label= c("Not in supply chain: 5.1", "Vertical: 2.7", "Direct: 13.2", "Indirect: 11.4")) +
-  scale_fill_manual(values= c("#B2182B", "#D6604D", "#F4A582", "#FDDBC7"), 
-                    breaks= c(7.1, 6.8, 19.2, 19.4),
-                    label= c("Not in supply chain: 7.1", "Vertical: 6.8", "Direct: 19.2", "Indirect: 19.4")) +
-  guides(linetype= guide_legend(order=1), color= guide_legend(order=2), fill= guide_legend(order=3)) +
-  labs(x="", y="Duration (months)", title="(d) Sourcing relationship", col="Median", fill= "Mean", linetype="") +
-  coord_cartesian(clip = 'off') +
+# Residuals plot
+model <- lm(Duration_lodged_closed ~ Complexity, table_analysis)
+summary(model)
+model %>% 
+  ggplot(aes(x= .fitted, y= .resid)) +
+  geom_point() +
+  geom_hline(yintercept = 0) +
+  labs(x= "Fitted duration values", y= "Residuals", title= "(c)") +
   theme(axis.title = element_text(size = 12), axis.text = element_text(size=11.5))
